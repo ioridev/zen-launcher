c2c36f0b94dc2f2924200c64d4cb794408b99991
Iori Kojima <iori@iori.dev>
2024-05-15 17:56:56 +0900

    Add search functionality to app list page

--- lib/app_list_page.dart
+++ lib/app_list_page.dart
@@ -12,6 +12,8 @@ class AppListPage extends StatefulWidget {
 
 class _AppListPageState extends State<AppListPage> {
   List<AppInfo> _appList = [];
+  List<AppInfo> _filteredAppList = [];
+  final TextEditingController _searchController = TextEditingController();
 
   @override
   void initState() {
@@ -23,24 +25,54 @@ class _AppListPageState extends State<AppListPage> {
     List<AppInfo> apps = await InstalledApps.getInstalledApps();
     setState(() {
       _appList = apps;
+      _filteredAppList = apps;
+    });
+  }
+
+  void _filterApps(String query) {
+    List<AppInfo> filteredApps = _appList
+        .where((app) => app.name.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+    setState(() {
+      _filteredAppList = filteredApps;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: ListView.builder(
-        itemCount: _appList.length,
-        itemBuilder: (context, index) {
-          AppInfo app = _appList[index];
-          return ListTile(
-            title: Text(app.name),
-            onTap: () async {
-              InstalledApps.startApp(app.packageName);
-            },
-          );
-        },
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _searchController,
+                decoration: const InputDecoration(
+                  hintText: 'Search apps...',
+                ),
+                onChanged: (value) {
+                  _filterApps(value);
+                },
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _filteredAppList.length,
+                itemBuilder: (context, index) {
+                  AppInfo app = _filteredAppList[index];
+                  return ListTile(
+                    title: Text(app.name),
+                    onTap: () {
+                      InstalledApps.startApp(app.packageName);
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
