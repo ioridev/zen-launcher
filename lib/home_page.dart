561eb74db02e3ee3f5fcc4d3a16daf365ecbe13a
Iori Kojima <iori@iori.dev>
2024-05-15 18:25:53 +0900

    Add Flutter Analog Clock widget to home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_analog_clock/flutter_analog_clock.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,10 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        color: Colors.black,
+        child: const Column(
+          children: [AnalogClock.dark()],
+        ));
   }
 }
