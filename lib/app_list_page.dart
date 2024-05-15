b20cdbbdfb49c7ea016d6d05e7a791300988ca70
Iori Kojima <iori@iori.dev>
2024-05-15 18:05:39 +0900

    Refactor app list page and add alphabet scroll bar

--- lib/app_list_page.dart
+++ lib/app_list_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:installed_apps/app_info.dart';
+import 'main.dart';
 import 'package:installed_apps/installed_apps.dart';
 
 class AppListPage extends StatefulWidget {
@@ -13,6 +14,7 @@ class _AppListPageState extends State<AppListPage> {
   List<AppInfo> _appList = [];
   List<AppInfo> _filteredAppList = [];
   final TextEditingController _searchController = TextEditingController();
+  final ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
@@ -37,41 +39,99 @@ class _AppListPageState extends State<AppListPage> {
     });
   }
 
+  void _scrollToIndex(int index) {
+    _scrollController.animateTo(
+      index * 56.0,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.black,
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: _searchController,
-                decoration: const InputDecoration(
-                  hintText: 'Search apps...',
+    return Scaffold(
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: [
+          SliverAppBar(
+            title: const Text('Launcher'),
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(48.0),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: TextField(
+                  controller: _searchController,
+                  decoration: const InputDecoration(
+                    hintText: 'Search apps...',
+                  ),
+                  onChanged: (value) {
+                    _filterApps(value);
+                  },
                 ),
-                onChanged: (value) {
-                  _filterApps(value);
-                },
               ),
             ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: _filteredAppList.length,
-                itemBuilder: (context, index) {
-                  AppInfo app = _filteredAppList[index];
-                  return ListTile(
-                    title: Text(app.name),
-                    onTap: () {
-                      InstalledApps.startApp(app.packageName);
-                    },
-                  );
-                },
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                AppInfo app = _filteredAppList[index];
+                return ListTile(
+                  title: Text(app.name),
+                  onTap: () {
+                    InstalledApps.startApp(app.packageName);
+                  },
+                );
+              },
+              childCount: _filteredAppList.length,
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: AlphabetScrollBar(
+        onTap: _scrollToIndex,
+        filteredAppList: _filteredAppList,
+      ),
+    );
+  }
+}
+
+class AlphabetScrollBar extends StatelessWidget {
+  final Function(int) onTap;
+  final List<AppInfo> filteredAppList;
+
+  const AlphabetScrollBar({
+    Key? key,
+    required this.onTap,
+    required this.filteredAppList,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerRight,
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          for (var letter in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''))
+            GestureDetector(
+              onTap: () {
+                int index = filteredAppList.indexWhere(
+                  (app) => app.name.toUpperCase().startsWith(letter),
+                );
+                if (index != -1) {
+                  onTap(index);
+                }
+              },
+              child: Text(
+                letter,
+                style: const TextStyle(
+                  fontSize: 12.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ],
-        ),
+        ],
       ),
     );
   }
